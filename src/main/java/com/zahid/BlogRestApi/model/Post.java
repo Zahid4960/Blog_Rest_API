22e541c3afa942f61ee1f7fb48ce51bc9b2603fd
zahid4960 <zahidcse4960@gmail.com>
2020-10-25 16:38:33 +0600

    added post model

---
+++ src/main/java/com/zahid/BlogRestApi/model/Post.java
@@ -0,0 +1,97 @@
+package com.zahid.BlogRestApi.model;
+
+public class Post {
+    private int id;
+    private String title;
+    private String description;
+    private int like;
+    private int dislike;
+    private int status;
+    private int user_id;
+    private String created_at;
+    private String updated_at;
+
+    public Post(int id, String title, String description, int like, int dislike, int status, int user_id, String created_at, String updated_at) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.like = like;
+        this.dislike = dislike;
+        this.status = status;
+        this.user_id = user_id;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public int getDislike() {
+        return dislike;
+    }
+
+    public void setDislike(int dislike) {
+        this.dislike = dislike;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}
