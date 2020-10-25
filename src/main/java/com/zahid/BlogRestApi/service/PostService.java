491345cd53f53421d0565be922074461386ffd8f
zahid4960 <zahidcse4960@gmail.com>
2020-10-25 17:10:39 +0600

    added post service

---
+++ src/main/java/com/zahid/BlogRestApi/service/PostService.java
@@ -0,0 +1,30 @@
+package com.zahid.BlogRestApi.service;
+
+import com.zahid.BlogRestApi.model.Post;
+import com.zahid.BlogRestApi.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public Post savePost(Post post){
+        return repository.save(post);
+    }
+
+    public List<Post> saveMultiplePosts(List<Post> posts){
+        return repository.saveAll(posts);
+    }
+
+    public List<Post> getAllPosts(){
+        return repository.findAll();
+    }
+
+    public Post findPostById(int id){
+        return repository.findById(id).orElse(null);
+    }
+}
