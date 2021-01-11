8f33ce0b366201925298fdc30c45fcb18db78ba1
zahid4960 <zahidcse4960@gmail.com>
2021-01-11 11:40:04 +0600

    working with the java response entity and json hash map

--- src/main/java/com/zahid/BlogRestApi/Controller/CategoryController.java
+++ src/main/java/com/zahid/BlogRestApi/Controller/CategoryController.java
@@ -3,10 +3,14 @@ package com.zahid.BlogRestApi.Controller;
 import com.zahid.BlogRestApi.model.Category;
 import com.zahid.BlogRestApi.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -15,22 +19,38 @@ public class CategoryController {
     CategoryService categoryService;
 
     @GetMapping("/api/v1/category")
-    public List<Category> index(){
-        return categoryService.getAllCategories();
+    public ResponseEntity<Map<String, Object>> index(){
+
+        List<Category> category_list = categoryService.getAllCategories();
+        Map<String, Object> message = new HashMap<String, Object>();
+
+        if(category_list.isEmpty()){
+            message.put("status", "success");
+            message.put("message", "No Data Found!!!");
+            message.put("data", category_list);
+
+            return new ResponseEntity(message, HttpStatus.OK);
+        }else{
+            message.put("status", "success");
+            message.put("message", "No Data Found!!!");
+            message.put("data", category_list);
+
+            return new ResponseEntity(message, HttpStatus.OK);
+        }
     }
 
     @PostMapping("/api/v1/category")
     public Category store(@RequestBody Category category){
         return categoryService.saveCategory(category);
     }
-
-    @GetMapping("/api/v1/category/{id}")
-    public Object show(@PathVariable Integer id){
-        return categoryService.findById(id);
-    }
-
-    @DeleteMapping("/api/v1/category/{id}")
-    public Object destroy(@PathVariable Integer id){
-        return categoryService.deleteById(id);
-    }
+//
+//    @GetMapping("/api/v1/category/{id}")
+//    public Object show(@PathVariable Integer id){
+//        return categoryService.findById(id);
+//    }
+//
+//    @DeleteMapping("/api/v1/category/{id}")
+//    public Object destroy(@PathVariable Integer id){
+//        return categoryService.deleteById(id);
+//    }
 }
--- src/main/java/com/zahid/BlogRestApi/service/CategoryService.java
+++ src/main/java/com/zahid/BlogRestApi/service/CategoryService.java
@@ -2,7 +2,6 @@ package com.zahid.BlogRestApi.service;
 
 import com.zahid.BlogRestApi.model.Category;
 import com.zahid.BlogRestApi.repository.CategoryRepository;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,36 +21,36 @@ public class CategoryService {
         return categoryRepository.save(category);
     }
 
-    public Object findById(Integer id){
-
-
-        JSONObject response = new JSONObject();
-//        JSONObject json_obj = new JSONObject();
-        String status;
-
-        Optional<Category> check_category_exists_or_not = categoryRepository.findById(id);
-
-        if(check_category_exists_or_not.isEmpty()){
-            status = "Category does not found!!";
-            response.put("status", status);
-            return response;
-        }else {
-            status = "Category found!!";
-            response.put("status", status);
-//            return categoryRepository.findById(id).get();
-            return response;
-        }
-    }
-
-    public Object deleteById(Integer id){
-
-        Optional<Category> check_category_exists_or_not = categoryRepository.findById(id);
-
-        if(check_category_exists_or_not.isEmpty()){
-            return "Category does not found!!";
-        }else {
-            categoryRepository.deleteById(id);
-            return "Category deleted successfully!!!";
-        }
-    }
+//    public Object findById(Integer id){
+//
+//
+//        JSONObject response = new JSONObject();
+////        JSONObject json_obj = new JSONObject();
+//        String status;
+//
+//        Optional<Category> check_category_exists_or_not = categoryRepository.findById(id);
+//
+//        if(check_category_exists_or_not.isEmpty()){
+//            status = "Category does not found!!";
+//            response.put("status", status);
+//            return response;
+//        }else {
+//            status = "Category found!!";
+//            response.put("status", status);
+////            return categoryRepository.findById(id).get();
+//            return response;
+//        }
+//    }
+//
+//    public Object deleteById(Integer id){
+//
+//        Optional<Category> check_category_exists_or_not = categoryRepository.findById(id);
+//
+//        if(check_category_exists_or_not.isEmpty()){
+//            return "Category does not found!!";
+//        }else {
+//            categoryRepository.deleteById(id);
+//            return "Category deleted successfully!!!";
+//        }
+//    }
 }
